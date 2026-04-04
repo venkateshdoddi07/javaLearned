bf3fdb8c0c3672a4967b7413406d6365e19cef9d
venkatesh doddi <doddi.venkatesh07@gmail.com>
2026-04-04 21:04:51 +0530

    changes for christmas tree pattern

--- javaPractice/src/com/codegnan/PatternChristmasTree.java
+++ javaPractice/src/com/codegnan/PatternChristmasTree.java
@@ -1,44 +1,49 @@
 package com.codegnan;
 
-import java.util.Scanner;
-
 public class PatternChristmasTree {
-	//value 5 is permanatly provided to the height variable
-	public static final int height=5;
-
-	public static void main(String[] args) {
-		Scanner scn =new Scanner(System.in);
-//assigning width
-		int width=5;
-		int space=2*width;
-		
-		int x=1;
-		for(int a=x;a<=height;a++) {
-			
-			for(int i=space;i>=x;i++ ) {
-				
-				System.out.println(" ");
-			}
-			
-			for(int j=1;j<=j;j++) {
-				
-				System.out.println("* ");
-			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-		}
-		
-
-	
-
-scn.close();
-	}
-
+	// value 5 is permenetly provided to the height variable.
+	public static final int height = 5;
+	   // Main Function (starting point of program execution)
+	   public static void main(String[] args) {
+	       // Assigning initial width of the pyramid
+	       int width = 5;
+	       // Assigning space value (used for alignment/indentation of tree)
+	       int space = width * 5;
+	       // Variable to control starting point of inner loop
+	       int x = 1;
+	       // Loop to print upper part of the tree (multiple pyramids)
+	       for (int a = 1; a <= height; a++) {//2<=5
+	    	   //inner for loop
+	           // Loop to control width of each pyramid level
+	           for (int i = x; i <= width; i++) {//1<=5
+	               // Loop to print spaces before stars (for center alignment)
+	               for (int j = space; j >= i; j--) {
+	                   System.out.print(" "); // printing space
+	               }
+	               // Loop to print stars in each row
+	               for (int k = 1; k <= i; k++) {
+	                   System.out.print("* "); // printing star with space
+	               }
+	               // Move to next line after printing one row
+	               System.out.println();
+	           }
+	           // Increasing starting point for next pyramid layer
+	           x = x + 2;
+	           // Increasing width to make next pyramid wider
+	           width = width + 2;
+	       }
+	       // Printing the trunk (branch) of the Christmas tree
+	       for (int i = 1; i <= 4; i++) {
+	           // Printing spaces before trunk to center it
+	           for (int j = space - 3; j >= 1; j--) {
+	               System.out.print(" ");
+	           }
+	           // Printing stars for trunk (fixed width)
+	           for (int k = 1; k <= 4; k++) {
+	               System.out.print("* ");
+	           }
+	           // Move to next line after each trunk row
+	           System.out.println();
+	       }
+	   }
 }
