30c32f63bbc87d06fd1ed867cf350d078c618ae7
venkatesh doddi <doddi.venkatesh07@gmail.com>
2026-04-28 23:52:52 +0530

    changes

--- javaPractice/src/com/codegnan/stringPrograms/ArrayListExample.java
+++ javaPractice/src/com/codegnan/stringPrograms/ArrayListExample.java
@@ -13,12 +13,13 @@ public class ArrayListExample {
 		studentList.add(new Student(105,"arjun",37));
 		studentList.add(new Student(110,"venk",24));
 		studentList.add(new Student(102,"pras",27));
+		
+		
+		studentList.add(0,new Student (111,"mahi",22));
 		System.out.println(studentList);
-		
-		
 
 
-
+//example with custom object home work
 		
 	}
 
