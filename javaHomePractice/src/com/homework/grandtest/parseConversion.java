6e6463ee5c6fce278cff15d9cd2c0d30cda928d8
venkatesh doddi <doddi.venkatesh07@gmail.com>
2026-05-11 19:00:06 +0530

    revision

--- javaHomePractice/src/com/homework/grandtest/parseConversion.java
+++ javaHomePractice/src/com/homework/grandtest/parseConversion.java
@@ -1,9 +1,26 @@
 package com.homework.grandtest;
+import java.util.Scanner;
+class StringToInt{
+	static void ParseNumber(String s) {
+		try {
+			double num=Double.parseDouble(s);
+			System.out.println("parsed number: "+num);
+			
+		}catch(NumberFormatException e) {
+			
+			System.out.println("Invalid number format");
+		}
+	}
+}
 
 public class parseConversion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		
+		String str=scn.nextLine();
+		StringToInt result=new StringToInt();
+		result.ParseNumber(str);
 
 	}
 
